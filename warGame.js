de79bcfc6746ea725a8b7a730ee7668d9404ff60
Nuno Gil Mesquita <nunomesquita2@gmail.com>
2023-04-12 15:39:25 +0100

    Work logic finished

--- warGame.js
+++ warGame.js
@@ -40,7 +40,6 @@ function splitDecks() {
 }
 
 function play() {
-  //<button onclick="play()">DRAW CARDS</button>
   drawCards();
   result();
   endGame();
@@ -48,30 +47,21 @@ function play() {
   player2Card = [];
 }
 
-function endGame() {
-  let winner = "";
-  if (player2Deck.length === 0) {
-    winner = "Player 1 wins!";
-    return true;
-  } else if (player1Deck.length === 0) {
-    winner = "Player 2 wins!";
-    return true;
-  } else return false;
-}
-
 function drawCards() {
   player1Card.push(player1Deck.shift());
+  document.getElementById("card1").innerHTML = renderCard(player1Card.at(-1));
   player2Card.push(player2Deck.shift());
+  document.getElementById("card2").innerHTML = renderCard(player2Card.at(-1));
 }
 
 function result() {
   if (player1Card.at(-1).power === player2Card.at(-1).power) {
     war();
-    return;
-  } player1Card.at(-1).power > player2Card.at(-1).power ? player1Deck.push(player2Card, player1Card) : player2Deck.push(player1Card, player2Card);
+  } else player1Card.at(-1).power > player2Card.at(-1).power ? player1Deck.push(...player2Card) && player1Deck.push(...player1Card) : player2Deck.push(...player1Card) && player2Deck.push(...player2Card);
 }
 
 function war() {
+  // Pausar para se clicar novamente em "Draw Cards"
   for (let i = 0; i < 4; i++) {
     if (player1Deck.length === 0 || player1Deck.length === 0) {
       return;
@@ -81,34 +71,76 @@ function war() {
   result();
 }
 
-function suitSymbol(card) {
-  switch (card.suit) {
-    case "hearts":
-      card.suit = "♥";
-      break;
+function endGame() {
+  p1score();
+  p2score();
+  if (player2Deck.length === 0) {
+    document.getElementById("winner").innerHTML = "Player 1 wins";
+  } else if (player1Deck.length === 0) {
+    document.getElementById("winner").innerHTML = "Player 2 wins";
   }
 }
 
 const images = {
   'hearts': {
-    'A': 'img/ace_of_hearts.png',
-    '2': 'img/2_of_hearts.png',
-    // ... 
+    'A': 'img/cards/hearts/ace_of_hearts.svg',
+    '2': 'img/cards/hearts/2_of_hearts.svg',
+    '3': 'img/cards/hearts/3_of_hearts.svg',
+    '4': 'img/cards/hearts/4_of_hearts.svg',
+    '5': 'img/cards/hearts/5_of_hearts.svg',
+    '6': 'img/cards/hearts/6_of_hearts.svg',
+    '7': 'img/cards/hearts/7_of_hearts.svg',
+    '8': 'img/cards/hearts/8_of_hearts.svg',
+    '9': 'img/cards/hearts/9_of_hearts.svg',
+    '10': 'img/cards/hearts/10_of_hearts.svg',
+    'J': 'img/jack.svg',
+    'Q': 'img/queen.svg',
+    'K': 'img/king.svg'
   },
   'diamonds': {
-    'A': 'img/ace_of_diamonds.png',
-    '2': 'img/2_of_diamonds.png',
-    // ... 
+    'A': 'img/cards/diamonds/ace_of_diamonds.svg',
+    '2': 'img/cards/diamonds/2_of_diamonds.svg',
+    '3': 'img/cards/diamonds/3_of_diamonds.svg',
+    '4': 'img/cards/diamonds/4_of_diamonds.svg',
+    '5': 'img/cards/diamonds/5_of_diamonds.svg',
+    '6': 'img/cards/diamonds/6_of_diamonds.svg',
+    '7': 'img/cards/diamonds/7_of_diamonds.svg',
+    '8': 'img/cards/diamonds/8_of_diamonds.svg',
+    '9': 'img/cards/diamonds/9_of_diamonds.svg',
+    '10': 'img/cards/diamonds/10_of_diamonds.svg',
+    'J': 'img/jack.svg',
+    'Q': 'img/queen.svg',
+    'K': 'img/king.svg'
   },
   'clubs': {
-    'A': 'img/ace_of_clubs.png',
-    '2': 'img/2_of_clubs.png',
-    // ... 
+    'A': 'img/cards/clubs/ace_of_clubs.svg',
+    '2': 'img/cards/clubs/2_of_clubs.svg',
+    '3': 'img/cards/clubs/3_of_clubs.svg',
+    '4': 'img/cards/clubs/4_of_clubs.svg',
+    '5': 'img/cards/clubs/5_of_clubs.svg',
+    '6': 'img/cards/clubs/6_of_clubs.svg',
+    '7': 'img/cards/clubs/7_of_clubs.svg',
+    '8': 'img/cards/clubs/8_of_clubs.svg',
+    '9': 'img/cards/clubs/9_of_clubs.svg',
+    '10': 'img/cards/clubs/10_of_clubs.svg',
+    'J': 'img/jack.svg',
+    'Q': 'img/queen.svg',
+    'K': 'img/king.svg'
   },
   'spades': {
-    'A': 'img/ace_of_spades.png',
-    '2': 'img/2_of_spades.png',
-    // ... 
+    'A': 'img/cards/spades/ace_of_spades.svg',
+    '2': 'img/cards/spades/2_of_spades.svg',
+    '3': 'img/cards/spades/3_of_spades.svg',
+    '4': 'img/cards/spades/4_of_spades.svg',
+    '5': 'img/cards/spades/5_of_spades.svg',
+    '6': 'img/cards/spades/6_of_spades.svg',
+    '7': 'img/cards/spades/7_of_spades.svg',
+    '8': 'img/cards/spades/8_of_spades.svg',
+    '9': 'img/cards/spades/9_of_spades.svg',
+    '10': 'img/cards/spades/10_of_spades.svg',
+    'J': 'img/jack.svg',
+    'Q': 'img/queen.svg',
+    'K': 'img/king.svg'
   }
 };
 
@@ -119,43 +151,31 @@ const symbols = {
   'spades': '♠'
 };
 
-const ranks = {
-  1: 'A',
-  2: '2',
-  3: '3',
-  4: '4',
-  5: '5',
-  6: '6',
-  7: '7',
-  8: '8',
-  9: '9',
-  10: '10',
-  11: 'J',
-  12: 'Q',
-  13: 'K'
-};
-
 function renderCard(card) {
-  const suit = card.suit;
-  const rank = ranks[card.rank];
-  const symbol = symbols[suit];
-  const imageSrc = images[suit][rank];
-  const cardHTML = `
-    <div class="card ${suit}">
+  const imageSrc = images[card.suit][card.value];
+  return cardHTML = `
+    <div class="card ${card.suit}">
         <div class="card-front">
           <div class="card-topleft">
-            <div class="card-corner-rank">${rank}</div>
-            <div class="card-corner-suit">${symbol}</div>
+            <div class="card-corner-rank">${card.value}</div>
+            <div class="card-corner-suit">${symbols[card.suit]}</div>
           </div>
           <div class="card-suits">
-            <img src="${imageSrc}" alt="${rank} of ${suit}">
+            <img src="${imageSrc}" alt="${card.value} of ${card.suit}">
           </div>
           <div class="card-bottomright">
-            <div class="card-corner-rank">${rank}</div>
-            <div class="card-corner-suit">${symbol}</div>
+            <div class="card-corner-rank">${card.value}</div>
+            <div class="card-corner-suit">${symbols[card.suit]}</div>
           </div>
         </div>
     </div>
   `;
-  return cardHTML;
+}
+
+function p1score() {
+  return document.getElementById("player1Score").innerHTML = player1Deck.length;
+}
+
+function p2score() {
+  return document.getElementById("player2Score").innerHTML = player2Deck.length;
 }
