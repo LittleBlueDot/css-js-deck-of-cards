02df3535b6587dbf0b4fb5fd49c9e518c16dd3ab
Lina Balciunaite <lina_balciunaite@yahoo.com>
2023-04-13 00:26:48 +0100

    final adjustments

--- warGame.js
+++ warGame.js
@@ -1,19 +1,33 @@
 const deck = [];
-const suits = ["hearts", "diamonds", "clubs", "spades"];
-const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+const values = [
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+  'A',
+];
 let player1Deck = [];
 let player2Deck = [];
 let player1Card = [];
 let player2Card = [];
 
 function startGame() {
-  document.querySelector(".game-scene").style.display = "flex"; // LBA added to show game scene
-  gameHeader.classList.toggle("fade"); // LBA
-  document.querySelector(".start-game").style.display = "none"; // LBA
-  document.querySelector(".restart").style.display = "none"; // LBA
+  document.querySelector('.game-scene').style.display = 'flex'; // LBA added to show game scene
+  gameHeader.classList.toggle('fade'); // LBA
+  document.querySelector('.start-game').style.display = 'none'; // LBA
+  document.querySelector('.restart').style.display = 'none'; // LBA
 
   //TODO: see if possible to combine multiple classes, the code bellow doesn´t work
-/*   const hideButtons = document.querySelectorAll(".start-game, .restart");
+  /*   const hideButtons = document.querySelectorAll(".start-game, .restart");
   hideButtons.style.display = "none"; */
   createDeck();
   shuffleDeck();
@@ -22,14 +36,13 @@ function startGame() {
 
 // LBA
 function restartGame() {
-  document.querySelector(".restart").style.display = "none"; // LBA
-  document.querySelector(".play").style.display = "block"; // LBA
-  document.querySelector(".text").style.display = "none"; // LBA
+  document.querySelector('.restart').style.display = 'none'; // LBA
+  document.querySelector('.play').style.display = 'block'; // LBA
+  document.querySelector('.text').style.display = 'none'; // LBA
   createDeck();
   shuffleDeck();
   splitDecks();
 }
-
 
 function createDeck() {
   for (let i = 0; i < suits.length; i++) {
@@ -37,8 +50,8 @@ function createDeck() {
       const card = {
         value: values[j],
         suit: suits[i],
-        power: values.indexOf(values[j])
-      }
+        power: values.indexOf(values[j]),
+      };
       deck.push(card);
     }
   }
@@ -67,15 +80,18 @@ function play() {
 
 function drawCards() {
   player1Card.push(player1Deck.shift());
-  document.getElementById("card1").innerHTML = renderCard(player1Card.at(-1));
+  document.getElementById('card1').innerHTML = renderCard(player1Card.at(-1));
   player2Card.push(player2Deck.shift());
-  document.getElementById("card2").innerHTML = renderCard(player2Card.at(-1));
+  document.getElementById('card2').innerHTML = renderCard(player2Card.at(-1));
 }
 
 function result() {
   if (player1Card.at(-1).power === player2Card.at(-1).power) {
     war();
-  } else player1Card.at(-1).power > player2Card.at(-1).power ? player1Deck.push(...player2Card) && player1Deck.push(...player1Card) : player2Deck.push(...player1Card) && player2Deck.push(...player2Card);
+  } else
+    player1Card.at(-1).power > player2Card.at(-1).power
+      ? player1Deck.push(...player2Card) && player1Deck.push(...player1Card)
+      : player2Deck.push(...player1Card) && player2Deck.push(...player2Card);
 }
 
 function war() {
@@ -93,89 +109,89 @@ function endGame() {
   p1score();
   p2score();
   if (player2Deck.length === 0) {
-    document.getElementById("winner").innerHTML = "Player 1 wins";
-    document.querySelector(".play").style.display = "none"; // LBA
-    document.querySelector(".restart").style.display = "block"; // LBA
+    document.getElementById('winner').innerHTML = 'Player 1 wins';
+    document.querySelector('.play').style.display = 'none'; // LBA
+    document.querySelector('.restart').style.display = 'block'; // LBA
   } else if (player1Deck.length === 0) {
-    document.getElementById("winner").innerHTML = "Player 2 wins";
-    document.querySelector(".play").style.display = "none"; // LBA
-    document.querySelector(".restart").style.display = "block"; // LBA
+    document.getElementById('winner').innerHTML = 'Player 2 wins';
+    document.querySelector('.play').style.display = 'none'; // LBA
+    document.querySelector('.restart').style.display = 'block'; // LBA
   }
 }
 
 const images = {
-  'hearts': {
-    'A': 'img/cards/hearts/ace_of_hearts.svg',
-    '2': 'img/cards/hearts/2_of_hearts.svg',
-    '3': 'img/cards/hearts/3_of_hearts.svg',
-    '4': 'img/cards/hearts/4_of_hearts.svg',
-    '5': 'img/cards/hearts/5_of_hearts.svg',
-    '6': 'img/cards/hearts/6_of_hearts.svg',
-    '7': 'img/cards/hearts/7_of_hearts.svg',
-    '8': 'img/cards/hearts/8_of_hearts.svg',
-    '9': 'img/cards/hearts/9_of_hearts.svg',
-    '10': 'img/cards/hearts/10_of_hearts.svg',
-    'J': 'img/jack.svg',
-    'Q': 'img/queen.svg',
-    'K': 'img/king.svg'
+  hearts: {
+    A: 'img/cards/hearts/ace_of_hearts.svg',
+    2: 'img/cards/hearts/2_of_hearts.svg',
+    3: 'img/cards/hearts/3_of_hearts.svg',
+    4: 'img/cards/hearts/4_of_hearts.svg',
+    5: 'img/cards/hearts/5_of_hearts.svg',
+    6: 'img/cards/hearts/6_of_hearts.svg',
+    7: 'img/cards/hearts/7_of_hearts.svg',
+    8: 'img/cards/hearts/8_of_hearts.svg',
+    9: 'img/cards/hearts/9_of_hearts.svg',
+    10: 'img/cards/hearts/10_of_hearts.svg',
+    J: 'img/jack.svg',
+    Q: 'img/queen.svg',
+    K: 'img/king.svg',
   },
-  'diamonds': {
-    'A': 'img/cards/diamonds/ace_of_diamonds.svg',
-    '2': 'img/cards/diamonds/2_of_diamonds.svg',
-    '3': 'img/cards/diamonds/3_of_diamonds.svg',
-    '4': 'img/cards/diamonds/4_of_diamonds.svg',
-    '5': 'img/cards/diamonds/5_of_diamonds.svg',
-    '6': 'img/cards/diamonds/6_of_diamonds.svg',
-    '7': 'img/cards/diamonds/7_of_diamonds.svg',
-    '8': 'img/cards/diamonds/8_of_diamonds.svg',
-    '9': 'img/cards/diamonds/9_of_diamonds.svg',
-    '10': 'img/cards/diamonds/10_of_diamonds.svg',
-    'J': 'img/jack.svg',
-    'Q': 'img/queen.svg',
-    'K': 'img/king.svg'
+  diamonds: {
+    A: 'img/cards/diamonds/ace_of_diamonds.svg',
+    2: 'img/cards/diamonds/2_of_diamonds.svg',
+    3: 'img/cards/diamonds/3_of_diamonds.svg',
+    4: 'img/cards/diamonds/4_of_diamonds.svg',
+    5: 'img/cards/diamonds/5_of_diamonds.svg',
+    6: 'img/cards/diamonds/6_of_diamonds.svg',
+    7: 'img/cards/diamonds/7_of_diamonds.svg',
+    8: 'img/cards/diamonds/8_of_diamonds.svg',
+    9: 'img/cards/diamonds/9_of_diamonds.svg',
+    10: 'img/cards/diamonds/10_of_diamonds.svg',
+    J: 'img/jack.svg',
+    Q: 'img/queen.svg',
+    K: 'img/king.svg',
   },
-  'clubs': {
-    'A': 'img/cards/clubs/ace_of_clubs.svg',
-    '2': 'img/cards/clubs/2_of_clubs.svg',
-    '3': 'img/cards/clubs/3_of_clubs.svg',
-    '4': 'img/cards/clubs/4_of_clubs.svg',
-    '5': 'img/cards/clubs/5_of_clubs.svg',
-    '6': 'img/cards/clubs/6_of_clubs.svg',
-    '7': 'img/cards/clubs/7_of_clubs.svg',
-    '8': 'img/cards/clubs/8_of_clubs.svg',
-    '9': 'img/cards/clubs/9_of_clubs.svg',
-    '10': 'img/cards/clubs/10_of_clubs.svg',
-    'J': 'img/jack.svg',
-    'Q': 'img/queen.svg',
-    'K': 'img/king.svg'
+  clubs: {
+    A: 'img/cards/clubs/ace_of_clubs.svg',
+    2: 'img/cards/clubs/2_of_clubs.svg',
+    3: 'img/cards/clubs/3_of_clubs.svg',
+    4: 'img/cards/clubs/4_of_clubs.svg',
+    5: 'img/cards/clubs/5_of_clubs.svg',
+    6: 'img/cards/clubs/6_of_clubs.svg',
+    7: 'img/cards/clubs/7_of_clubs.svg',
+    8: 'img/cards/clubs/8_of_clubs.svg',
+    9: 'img/cards/clubs/9_of_clubs.svg',
+    10: 'img/cards/clubs/10_of_clubs.svg',
+    J: 'img/jack.svg',
+    Q: 'img/queen.svg',
+    K: 'img/king.svg',
   },
-  'spades': {
-    'A': 'img/cards/spades/ace_of_spades.svg',
-    '2': 'img/cards/spades/2_of_spades.svg',
-    '3': 'img/cards/spades/3_of_spades.svg',
-    '4': 'img/cards/spades/4_of_spades.svg',
-    '5': 'img/cards/spades/5_of_spades.svg',
-    '6': 'img/cards/spades/6_of_spades.svg',
-    '7': 'img/cards/spades/7_of_spades.svg',
-    '8': 'img/cards/spades/8_of_spades.svg',
-    '9': 'img/cards/spades/9_of_spades.svg',
-    '10': 'img/cards/spades/10_of_spades.svg',
-    'J': 'img/jack.svg',
-    'Q': 'img/queen.svg',
-    'K': 'img/king.svg'
-  }
+  spades: {
+    A: 'img/cards/spades/ace_of_spades.svg',
+    2: 'img/cards/spades/2_of_spades.svg',
+    3: 'img/cards/spades/3_of_spades.svg',
+    4: 'img/cards/spades/4_of_spades.svg',
+    5: 'img/cards/spades/5_of_spades.svg',
+    6: 'img/cards/spades/6_of_spades.svg',
+    7: 'img/cards/spades/7_of_spades.svg',
+    8: 'img/cards/spades/8_of_spades.svg',
+    9: 'img/cards/spades/9_of_spades.svg',
+    10: 'img/cards/spades/10_of_spades.svg',
+    J: 'img/jack.svg',
+    Q: 'img/queen.svg',
+    K: 'img/king.svg',
+  },
 };
 
 const symbols = {
-  'hearts': '♥',
-  'diamonds': '♦',
-  'clubs': '♣',
-  'spades': '♠'
+  hearts: '♥',
+  diamonds: '♦',
+  clubs: '♣',
+  spades: '♠',
 };
 
 function renderCard(card) {
   const imageSrc = images[card.suit][card.value];
-  return cardHTML = `
+  return (cardHTML = `
     <div class="card ${card.suit}">
         <div class="card-front">
           <div class="card-topleft">
@@ -191,13 +207,15 @@ function renderCard(card) {
           </div>
         </div>
     </div>
-  `;
+  `);
 }
 
 function p1score() {
-  return document.getElementById("player1Score").innerHTML = player1Deck.length;
+  return (document.getElementById('player1Score').innerHTML =
+    player1Deck.length);
 }
 
 function p2score() {
-  return document.getElementById("player2Score").innerHTML = player2Deck.length;
+  return (document.getElementById('player2Score').innerHTML =
+    player2Deck.length);
 }
